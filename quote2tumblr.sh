50005a46a23d2f298c19a525167b50ea31660ef1
Anton Yakutovich <drakulavich@gmail.com>
2011-08-05 01:14:15 +0300

    change comments

--- quote2tumblr.sh
+++ quote2tumblr.sh
@@ -8,18 +8,18 @@ TMB_TYPE='quote';
 
 # Get quotes
 CLIPPINGSFILE='/mnt/us/documents/My Clippings.txt';
-if [ -f "$CLIPPINGSFILE" ]; then # существует ли файл?
+if [ -f "$CLIPPINGSFILE" ]; then # file exists?
 	TMB_QUOTE=$(tail -n5 "$CLIPPINGSFILE" | sed -n '1!G;h;$p' | sed -n -e '1,/Highlight/p' | grep -v -e 'Highlight' -e '===' | sed '$d');
 	TMB_SRC=$(tail -n5 "$CLIPPINGSFILE" | sed -n '1!G;h;$p' | grep -m1 -A1 "Highlight" | tail -1);
 else
 CLIPPINGSFILE='/mnt/us/documents/Мои вырезки.txt';
-if [ -f "$CLIPPINGSFILE" ]; then # существует ли файл?
+if [ -f "$CLIPPINGSFILE" ]; then # file exists?
 	TMB_QUOTE=$(tail -n5 "$CLIPPINGSFILE" | sed -n '1!G;h;$p' | sed -n -e '1,/Выделение/p' | grep -v -e 'Выделение' -e '===' | sed '$d');
 	TMB_SRC=$(tail -n5 "$CLIPPINGSFILE" | sed -n '1!G;h;$p' | grep -m1 -A1 "Выделение" | tail -1);
 fi
 fi
 
-# Updating
+# Post quote to blog
 if [ "$TMB_SRC" != "" ]; then
 	curl -s \
 	-d "email=$TMB_USER&password=$TMB_PASS&type=$TMB_TYPE&quote=$TMB_QUOTE&source=$TMB_SRC&generator=shell" \
